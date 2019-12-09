8e191706a1d6c0eb096ae299c74e49c7c6affc9b
Themis Papavasileiou <tpapavasileiou@bol.com>
2019-12-09 10:02:31 +0100

    day 9 thanks reddit for refactoring day 7 and abstracting the mode resolution!11!1

---
+++ 9/part1.py
@@ -0,0 +1,71 @@
+opcodes = list(map(int, open('input/9.txt').read().strip().split(',')))
+
+
+def machine():
+    p = opcodes[:] + [0] * 8000000  # grab a fresh one + extra mem ghetto way
+    ip = 0
+    rel_base = 0
+    while True:
+        cmd = p[ip]
+        op = cmd % 100
+        # C B A .. modes can be 0 1 or 2
+        mode = [cmd // 100 % 10, cmd // 1000 % 10, cmd // 10000 % 10]
+        values = [0, 0, 0]
+        # positions of A B C
+        pos = p[ip + 1:ip + 4]
+
+        # this is from reddit too. Making it a generator and abstracting or the if else out of every op is the lawd's work
+        for i in range(3):
+            if mode[i] == 0:
+                values[i] = p[pos[i]]
+            elif mode[i] == 2:
+                pos[i] = rel_base + pos[i]
+                values[i] = p[pos[i]]
+            else:
+                values[i] = pos[i]
+                pos[i] = ip + i + 1
+
+        a, b, c = values
+
+        if op == 1:
+            p[pos[2]] = a + b
+            ip += 4
+        elif op == 2:
+            p[pos[2]] = a * b
+            ip += 4
+        elif op == 3:
+            p[pos[0]] = yield  # wait for input, so state-machiney
+            ip += 2
+        elif op == 4:
+            yield a
+            ip += 2
+        elif op == 5:
+            if a != 0:
+                ip = b
+            else:
+                ip += 3
+        elif op == 6:
+            if a == 0:
+                ip = b
+            else:
+                ip += 3
+        elif op == 7:
+            p[pos[2]] = 1 if a < b else 0
+            ip += 4
+        elif op == 8:
+            p[pos[2]] = 1 if a == b else 0
+            ip += 4
+        elif op == 9:
+            rel_base += a
+            ip += 2
+        elif op == 99:
+            break
+
+
+m = machine()
+next(m)
+print(m.send(1))
+
+m = machine()
+next(m)
+print(m.send(2))
