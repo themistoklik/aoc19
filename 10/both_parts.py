c86947eafc046384678b3e5a8867f71936075e0f
Themis Papavasileiou <tpapavasileiou@bol.com>
2019-12-10 16:15:49 +0100

    day 10 realize teh correct coordinate system..

--- 10/both_parts.py
+++ 10/both_parts.py
@@ -18,7 +18,7 @@ def part_1(board):
             if point == other_point: continue
             # colinear? they'll have the same angle with the axes
             slope_between_two_points = atan2(other_point[1] - point[1], other_point[0] - point[0])
-            # https://gamedev.stackexchange.com/a/69678
+            # https://gamedev.stackexchange.com/a/69678, also top left is 0,0 so -90 is where we start
             if slope_between_two_points < radians(-90):
                 slope_between_two_points = abs(slope_between_two_points+2*pi)
             slope_between_two_points = degrees(slope_between_two_points)
@@ -29,6 +29,12 @@ def part_1(board):
 
     max_point = max(points, key=lambda x: len(
         points[x].values()))  # how many slopes you got is how many points fall in same line
+    # every point now sees other points
+    for point in points.keys():
+        for slope in points[point].keys():
+            points[point][slope].sort(
+                key=lambda other_point: -(abs(point[0] - other_point[0]) + abs(point[1] - other_point[1])))
+
     return points, max_point
 
 
@@ -37,22 +43,15 @@ points, max_point = part_1(board)
 print(len(points[max_point].values()))
 
 # part2
-evaporation = 0
-
-for point in points.keys():
-    for slope in points[point].keys():
-        points[point][slope].sort(key=lambda other_point: abs(point[0] - other_point[0]) + abs(point[1] - other_point[1]))
-#now each point for all its slopes "sees" the other points in order
-
 evaporations = 0
 pop = True
 
-while pop:
+while pop: #spin spin spin
     pop = False
     for slope in sorted(points[max_point].keys()):
         if points[max_point][slope]:
             evaporations += 1
-            target = points[max_point][slope].pop(0) #pop gets the last one (most far out) so they need to be sorted asc
+            target = points[max_point][slope].pop() #pop() gets the last one so they need to be distance sorted
         if evaporations == 200:
             pop = True
             print(target)
